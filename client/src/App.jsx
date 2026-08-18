f2f5172350fc7ca904bc3e82a1a7aa2e617daaa9
Sujal Kumbhare <kumbharesujal16@gmail.com>
2026-08-18 11:58:03 +0530

    Fix frontend authentication and API configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,51 @@
-import React, { useEffect } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Auth from './pages/Auth'
-import axios from 'axios'
-import { useDispatch } from 'react-redux'
-import { setUserData } from './redux/userSlice'
-import InterviewPage from './pages/InterviewPage'
-import InterviewHistory from './pages/InterviewHistory'
-import Pricing from './pages/Pricing'
-import InterviewReport from './pages/InterviewReport'
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setUserData } from "./redux/userSlice";
+import InterviewPage from "./pages/InterviewPage";
+import InterviewHistory from "./pages/InterviewHistory";
+import Pricing from "./pages/Pricing";
+import InterviewReport from "./pages/InterviewReport";
 
-export const ServerUrl = import.meta.env.VITE_API_URL
+export const ServerUrl = import.meta.env.VITE_API_URL;
 
 function App() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const getUser = async () => {
       try {
+        if (!ServerUrl) {
+          console.error("VITE_API_URL is not defined");
+          dispatch(setUserData(null));
+          return;
+        }
+
         const result = await axios.get(
-          ServerUrl + '/api/user/current-user',
+          `${ServerUrl}/api/user/current-user`,
           {
-            withCredentials: true
+            withCredentials: true,
           }
-        )
+        );
 
-        dispatch(setUserData(result.data))
+        dispatch(setUserData(result.data));
       } catch (error) {
-        console.log('Current user error:', error)
-        dispatch(setUserData(null))
-      }
-    }
+        console.error("Current User Error:", error);
 
-    getUser()
-  }, [dispatch])
+        if (error.response) {
+          console.error("Status:", error.response.status);
+          console.error("Response:", error.response.data);
+        }
+
+        dispatch(setUserData(null));
+      }
+    };
+
+    getUser();
+  }, [dispatch]);
 
   return (
     <Routes>
@@ -44,7 +56,7 @@ function App() {
       <Route path="/pricing" element={<Pricing />} />
       <Route path="/report/:id" element={<InterviewReport />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
